cb871fe84e736bae2a4aa4135179982cf0f21402
Raphael Gomes <radago@hotmail.com.br>
2019-10-17 00:18:50 -0300

    Finalização da opção de nova série e adição das opções editar, excluir e comentar

--- controllers/series.js
+++ controllers/series.js
@@ -1,22 +1,75 @@
-const index = ({ Serie }, req, res) => {
-  Serie.find({}, (err, docs) => {
-    res.render("series/index", { series: docs });
-  });
+labels = [
+  { id: "to-watch", name: "Para assistir" },
+  { id: "watching", name: "Assistindo" },
+  { id: "watched", name: "Assistido" }
+];
+
+const index = async ({ Serie }, req, res) => {
+  const docs = await Serie.find({});
+  res.render("series/index", { series: docs, labels });
 };
-const novaProcess = ({ Serie }, req, res) => {
-  console.log(req.body);
+const novaProcess = async ({ Serie }, req, res) => {
   const serie = new Serie(req.body);
-  serie.save(_ => {
+  try {
+    await serie.save();
     res.redirect("/series");
-  });
+  } catch (e) {
+    res.render("series/nova", {
+      erros: Object.keys(e.errors)
+    });
+  }
 };
 
 const novaForm = (req, res) => {
-  res.render("series/nova");
+  res.render("series/nova", { erros: [] });
+};
+
+const excluir = async ({ Serie }, req, res) => {
+  await Serie.remove({ _id: req.params.id });
+  res.redirect("/series");
+};
+
+const editarProcess = async ({ Serie }, req, res) => {
+  const serie = await Serie.findOne({ _id: req.params.id });
+  serie.name = req.body.name;
+  serie.status = req.body.status;
+  try {
+    await serie.save();
+    res.redirect("/series");
+  } catch (e) {
+    res.render("series/editar", {
+      serie,
+      labels,
+      erros: Object.keys(e.errors)
+    });
+  }
+};
+
+const editarForm = async ({ Serie }, req, res) => {
+  const serie = await Serie.findOne({ _id: req.params.id });
+  res.render("series/editar", { serie, labels, erros: [] });
+};
+
+const info = async ({ Serie }, req, res) => {
+  const serie = await Serie.findOne({ _id: req.params.id });
+  res.render("series/info", { serie });
+};
+
+const addComentario = async ({ Serie }, req, res) => {
+  await Serie.updateOne(
+    { _id: req.params.id },
+    { $push: { comments: req.body.comentario } }
+  );
+  res.redirect(`/series/info/${req.params.id}`);
 };
 
 module.exports = {
   index,
   novaProcess,
-  novaForm
+  novaForm,
+  excluir,
+  editarForm,
+  editarProcess,
+  info,
+  addComentario
 };
--- routes/series.js
+++ routes/series.js
@@ -12,4 +12,12 @@ Router.get("/", seriesController.index.bind(null, models));
 Router.get("/nova", seriesController.novaForm);
 Router.post("/nova", seriesController.novaProcess.bind(null, models));
 
+Router.get("/excluir/:id", seriesController.excluir.bind(null, models));
+
+Router.get("/editar/:id", seriesController.editarForm.bind(null, models));
+Router.post("/editar/:id", seriesController.editarProcess.bind(null, models));
+
+Router.get("/info/:id", seriesController.info.bind(null, models));
+Router.post("/info/:id", seriesController.addComentario.bind(null, models));
+
 module.exports = Router;
