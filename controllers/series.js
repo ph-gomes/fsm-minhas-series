edd7d7ef33087bc1859fe6f056d03174f74ff297
Raphael Gomes <radago@hotmail.com.br>
2019-10-17 21:30:16 -0300

    Adição de paginação

--- controllers/series.js
+++ controllers/series.js
@@ -4,9 +4,31 @@ labels = [
   { id: "watched", name: "Assistido" }
 ];
 
+const pagination = async (model, conditions, params) => {
+  const total = await model.countDocuments(conditions);
+  const pageSize = parseInt(params.pageSize) || 1;
+  const currentPage = parseInt(params.page) || 0;
+
+  const pagination = {
+    currentPage,
+    pageSize,
+    pages: parseInt(total / pageSize)
+  };
+
+  results = await model
+    .find(conditions)
+    .limit(pageSize)
+    .skip(currentPage * pageSize);
+
+  return {
+    data: results,
+    pagination
+  };
+};
+
 const index = async ({ Serie }, req, res) => {
-  const docs = await Serie.find({});
-  res.render("series/index", { series: docs, labels });
+  const results = await pagination(Serie, {}, req.query);
+  res.render("series/index", { results, labels });
 };
 const novaProcess = async ({ Serie }, req, res) => {
   const serie = new Serie(req.body);
